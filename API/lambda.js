da5973b167ccdd1088e85e6f08160e4a6d2c5a1a
Michael Welling <welldog777@gmail.com>
2017-08-23 08:40:51 -0400

    Updated API for Node.js 6, use https for links

--- API/lambda.js
+++ API/lambda.js
@@ -1,83 +1,88 @@
-var Client = require('node-rest-client').Client;
+var https = require('https');
 var colorArray = ["orr1","orr2","orr3","orr4"];
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
 
-	var client = new Client();
-	
-	// You will probably want to use your own Google Spreadsheet here :)
-	client.get("https://docs.google.com/a/orrfellowship.org/spreadsheets/d/1DAJAojHrH2RNgP1HWojkPsbId6fWeCBDEtIUKHRNkDE/export?gid=1726067716&format=csv", function (data, response) {
+    var body = '';
 
-		var name = data.toString('utf8');
-		var dataArray = name.split('\n');
-		dataArray.splice(0,1);
+	  // You will probably want to use your own Google Spreadsheet here :)
+	  https.get(process.env.DOCS_API_URL, function (res) {
 
-		var output = [];
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
 
-		for (i = 0; i < dataArray.length; i++) {
-			var currentLine = dataArray[i].trim();
-			var components = currentLine.split(',');
+        res.on('end', function() {
 
-			// Date stuff to skip brags older than one month
-			var monthAgoDate = new Date();
-			if (monthAgoDate.getMonth() == 1) {
-				monthAgoDate.setMonth(12);
-				monthAgoDate.setFullYear(monthAgoDate.getFullYear() - 1);
-			} else {
-				monthAgoDate.setMonth(monthAgoDate.getMonth() - 1);
-			}
-			var bragDate = new Date(components[0]);
-			if (bragDate.getTime() < monthAgoDate.getTime()) {
-				continue;
-			}
+    		var name = body.toString('utf8');
+    		var dataArray = name.split('\n');
+    		dataArray.splice(0,1);
 
-			// Remove the timestamp and any quotes used to preserve CSV formatting
-			components.splice(0,1);
-			currentLine = components.join('|');
-			if(currentLine.charCodeAt(0) == 34) {
-				currentLine = currentLine.substring(1, currentLine.length - 1);
-			}
+    		var output = [];
 
-			var randColor = colorArray[Math.floor((Math.random() * colorArray.length-1) + 1)];
-			var dimentions = {'min':0,'max':0};
+    		for (i = 0; i < dataArray.length; i++) {
+    			var currentLine = dataArray[i].trim();
+    			var components = currentLine.split(',');
 
-			if (currentLine.length < 15) {
-				//sum 1
-				dimentions['min'] = 1;
-				dimentions['max'] = 1;
-			} else if (currentLine.length < 70) {
-				//sum 2
-				dimentions['min'] = 2;
-				dimentions['max'] = 2;
-			} else if (currentLine.length < 100) {
-				//sum 3
-				dimentions['min'] = 2;
-				dimentions['max'] = 2;
-			} else if (currentLine.length < 130) {
-				//sum 4
-				dimentions['min'] = 3;
-				dimentions['max'] = 1;
-			} else {
-				dimentions['min'] = 3;
-				dimentions['max'] = 1;
-			}
-			var randlength = Math.floor((Math.random() * dimentions['max']) + dimentions['min']);
-			var randWidth = Math.floor((Math.random() * dimentions['max']) + dimentions['min']);
-			var outputData = {'length':randlength,'width':randWidth,'color':randColor,'message':currentLine};
+    			// Date stuff to skip brags older than one month
+    			var monthAgoDate = new Date();
+    			if (monthAgoDate.getMonth() == 1) {
+    				monthAgoDate.setMonth(12);
+    				monthAgoDate.setFullYear(monthAgoDate.getFullYear() - 1);
+    			} else {
+    				monthAgoDate.setMonth(monthAgoDate.getMonth() - 1);
+    			}
+    			var bragDate = new Date(components[0]);
+    			if (bragDate.getTime() < monthAgoDate.getTime()) {
+    				continue;
+    			}
 
-			output.push(outputData);
-		}
+    			// Remove the timestamp and any quotes used to preserve CSV formatting
+    			components.splice(0,1);
+    			currentLine = components.join('|');
+    			if(currentLine.charCodeAt(0) == 34) {
+    				currentLine = currentLine.substring(1, currentLine.length - 1);
+    			}
 
-		var time = new Date();
-		console.log("Returned " + output.length + " values on " + time);
-		context.done(null, output); // SUCCESS with message
-	});
+    			var randColor = colorArray[Math.floor((Math.random() * colorArray.length-1) + 1)];
+    			var dimentions = {'min':0,'max':0};
 
-    
+    			if (currentLine.length < 15) {
+    				//sum 1
+    				dimentions['min'] = 1;
+    				dimentions['max'] = 1;
+    			} else if (currentLine.length < 70) {
+    				//sum 2
+    				dimentions['min'] = 2;
+    				dimentions['max'] = 2;
+    			} else if (currentLine.length < 100) {
+    				//sum 3
+    				dimentions['min'] = 2;
+    				dimentions['max'] = 2;
+    			} else if (currentLine.length < 130) {
+    				//sum 4
+    				dimentions['min'] = 3;
+    				dimentions['max'] = 1;
+    			} else {
+    				dimentions['min'] = 3;
+    				dimentions['max'] = 1;
+    			}
+    			var randlength = Math.floor((Math.random() * dimentions['max']) + dimentions['min']);
+    			var randWidth = Math.floor((Math.random() * dimentions['max']) + dimentions['min']);
+    			var outputData = {'length':randlength,'width':randWidth,'color':randColor,'message':currentLine};
+
+    			output.push(outputData);
+    		}
+
+    		var time = new Date();
+    		console.log("Returned " + output.length + " values on " + time);
+    		callback(null, output); // SUCCESS with message
+    	});
+    });
+
 };
 
 function shuffle(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
-
